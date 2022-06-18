c3f3a2af48e168675c56ab584bc1c7ccc29f75fa
kengzzzzz <kengzzzzz@gmail.com>
2022-06-18 13:16:01 +0700

    Update webhooks.php

--- webhooks.php
+++ webhooks.php
@@ -3,7 +3,7 @@
 require "vendor/autoload.php";
 require_once('vendor/linecorp/line-bot-sdk/line-bot-sdk-tiny/LINEBotTiny.php');
 
-$access_token = '3ALKAbKFoGuJyJnoDdn0HeyfbxLFtEXBKiC0lFeoNl/XbL4WhoCZzefp2n7UDuXaCWfErIDro07BnZNggJmXJChXTIlMPo8LRJ+n1LEgbRUaKehDkiCr5p5CakHrPX+gauOGX/R5bB2e5yi7xjnHDAdB04t89/1O/w1cDnyilFU=';
+$access_token = 'Oq1uGQTIP7dNs6/kbDwQCdC6viPi6np+QYrGM+CAHZBsQxZU6Q/NQjN9u9fxAeYGu7my/2cItI6Do8ZFRSQeTHYOGOHgQySCQ0ZwUxHIJJQzySDNaVzjHh8ABX6Ob4RPW42sfvVAdOO9Ss1X9ImH2wdB04t89/1O/w1cDnyilFU=';
 
 // Get POST body content
 $content = file_get_contents('php://input');
